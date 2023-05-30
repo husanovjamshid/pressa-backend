cd1466ec0986f2aedbe37b3be667af2f558ed8cb
husanovjamshid <husanovjamshid11@gmail.com>
2023-05-30 14:05:09 +0500

    add dotenv file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import {resolve} from 'path'
+import { resolve } from 'path';
+import 'dotenv/config';
+
+const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(resolve('uploads')))
+app.use(express.static(resolve('uploads')));
+
+app.listen(PORT, console.log('server is running port ' + PORT));
