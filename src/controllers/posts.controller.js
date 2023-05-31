9c685b4a48d9dfe822063d0d7863a66667308d1c
husanovjamshid <husanovjamshid11@gmail.com>
2023-05-31 12:29:14 +0500

    add multer

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,13 +1,27 @@
 import { hashPassword, write, read } from '../utils/model.js';
 import { resolve } from 'path';
+import upload from '../utils/multer.js';
 
 export const GET = (req, res, next) => {
 	const posts = read('posts');
 	const authors = read('author');
+	const category = read('category');
+	const subCategory = read('subCategory');
 
-	const checkPosts = posts.filter((post) => post.author_id);
+	posts.map((post) => {
+		post.author = authors.filter(
+			(author_item) => post.author_id == author_item.author_id,
+		);
+		post.category = category.filter(
+			(category_item) => post.category_id == category_item.category_id,
+		);
+		post.sub_category = subCategory.filter(
+			(subCategory_item) =>
+				post.sub_category_id == subCategory_item.sub_category_id,
+		);
+	});
 
-	res.status(200).json({ status: 200, data: checkPosts });
+	res.status(200).json({ status: 200, data: posts });
 };
 
 export const GET_BY_ID = (req, res, next) => {
@@ -19,17 +33,59 @@ export const GET_BY_ID = (req, res, next) => {
 		if (getPosts == undefined) {
 			throw new Error('Params not found');
 		}
+
+		getPosts.post_view_count += 1;
+
+		write('posts', posts);
 		res.status(200).json({ status: 200, data: getPosts });
 	} catch (error) {
-        res.status(400).json({ status: 400, message: error.message });
-
-    }
+		res.status(400).json({ status: 400, message: error.message });
+	}
 };
 
 export const POST = (req, res, next) => {
 	const posts = read('posts');
-	const { post_title, post_body, post_started_date, post_started_time } =
-		req.body;
+	const authors = read('author');
 
-	const { image } = req.file;
+	const { filename } = req.file;
+	const {
+		post_title,
+		post_desc,
+		post_link,
+		author_name,
+		author_number,
+		author_role,
+		post_time,
+		post_date,
+		post_type,
+		category_id,
+		sub_category_id,
+	} = req.body;
+
+	const newAuthor = {
+		author_id: authors.at(-1)?.author_id + 1 || 1,
+		author_name,
+		author_number,
+		author_role,
+	};
+	
+	const newPost = {
+		post_id: posts.at(-1)?.post_id + 1 || 1,
+		post_title,
+		post_desc,
+		post_link,
+		author_id: authors.at(-1)?.author_id + 1,
+		post_time,
+		post_img: filename,
+		post_date,
+		post_type,
+		post_view_count: 0,
+		category_id: +category_id,
+		sub_category_id: +sub_category_id,
+		post_status: 'uncomplated',
+	};
+
+	write('posts', [...posts, newPost]);
+	write('author', [...authors, newAuthor]);
+	res.send('uploads');
 };
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { GET, GET_BY_ID, POST } from '../controllers/posts.controller.js';
+import upload from '../utils/multer.js';
 
 const router = Router();
 
 router.get('/posts', GET);
 router.get('/posts/:id', GET_BY_ID)
-router.post('/ancement', POST);
+router.post('/posts',upload.single('image'), POST);
 
 export default router;
